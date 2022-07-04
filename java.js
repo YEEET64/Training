2ccfb31db5cb89511ca7e40b0f2e0e957b022631
YEEET64 <84526443+YEEET64@users.noreply.github.com>
2022-07-04 10:15:34 +0200

    Update java.js

--- java.js
+++ java.js
@@ -2,4 +2,5 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 var Zufall = getRandomInt(11);
-document.getElementById("number").value = Zufall;
+document.getElementById("number").value = 0;
+console.log(Zufall);
